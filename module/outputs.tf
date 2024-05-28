e31e9f82418434eb535a4ba1fb7a7823b5a6c95d
ajay1233tfhgdsjfhdg <ajayreddy1202@gmail.com>
2024-05-28 11:46:33 +0530

    Update outputs.tf

--- module/outputs.tf
+++ module/outputs.tf
@@ -1,11 +1,11 @@
 output "roles" {
-  value = module.iam.role_name
+  value = iam.role_name
 }
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = vpc.vpc_id
 }
 
 output "subnet_id" {
-  value = module.vpc.public_subnet_id
+  value = vpc.public_subnet_id
 }
