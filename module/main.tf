17a829b8c40d2539ea9f86d4e59b45de66a3abae
ajay1233tfhgdsjfhdg <ajayreddy1202@gmail.com>
2024-05-28 12:06:19 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -6,14 +6,14 @@
 
 
 module "s3" {
-  source = "./module/s3"
+  source = "./s3"
   for_each = var.bucket_name
   bucket_name = each.value.bucket
 }
 
 
 module "iam" {
-  source = "./module/iam"
+  source = "./iam"
   for_each = var.role
   role = each.value.role
 }
